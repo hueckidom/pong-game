8ad24cd1e2c4a06376287ad51f7972b5c2b09121
Hücki <dominic.hueckmann@mmmake.com>
2024-05-10 11:25:33 +0200

    adjust service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,34 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-app-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/static/js/main.chunk.js',
-          '/static/js/0.chunk.js',
-          '/static/js/bundle.js',
-          '/static/css/main.chunk.css',
-          '/favicon.ico',
-          '/logo192.png', // Cache other assets and images
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.css',
+        '/index.js',
+        '/favicon.ico',
+        '/logo192.png',
+        '/logo512.png',
+        '/manifest.json',
+        '/vite.svg',
+        '/assets/button-click-sound.mp3',
+        '/assets/correct.mp3',
+        '/assets/game.mp3',
+        '/assets/goal.mp3',
+        '/assets/Paddle Ball Hit Sound Effect HD.mp3',
+        '/assets/questions.mp3',
+        '/assets/wrong.mp3',
+        '/assets/correct.mp3',
+        '/assets/valuehero.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
